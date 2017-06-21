9ac455a2a1a69752b39311bf6aba5bef40a396a6
aaivanova <aa.ivanova.28@gmail.com>
2017-06-21 10:27:59 +0300

    New bg

--- app/src/main/java/com/example/user/miniteamsminigames/Tap_tap.java
+++ app/src/main/java/com/example/user/miniteamsminigames/Tap_tap.java
@@ -73,7 +73,7 @@ public class Tap_tap extends View{
     }
 
     private void restart() {
-        int random = (int) (Math.random() * w / 2 + w / 2);
+        int random = (int) (Math.random() * w + w / 4);
 //        int random = 200;
         Log.d("KEK111", Integer.toString(random));
         squares.add(new Square(- d / 2, - random + d / 2, d, random, bg2, true));
